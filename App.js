9f2535f05144f1c624b4812d21d043a029a00fd7
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-09 20:45:35 -0300

    Grupos de estilo com StyleSheet

--- App.js
+++ App.js
@@ -1,36 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: ''
-    }
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome) {
-    this.setState({
-      nome: nome
-    })
-  }
 
   render(){
 
     return(
-      <View>
-
-      <Button title={"Entrar"} onPress={() => this.entrar('Daniel Nascimento')}/>
-
-        <Text>
-          {this.state.nome}
-        </Text>
+      <View style={styles.area}>
+        <Text style={styles.txt1, styles.alinhaTexto}>Eu sou texto 1</Text>
+        <Text>Eu sou texto 2</Text>
+        <Text>Eu sou texto 3</Text>
+        <Text style={styles.alinhaTexto}>Eu sou texto 4</Text>
       </View>
     );
   }
 
 }
+
+const styles = StyleSheet.create({
+  area: {
+    marginTop: 20
+  },
+  txt1: {
+    fontSize: 25,
+    color: 'red'
+  },
+  alinhaTexto: {
+    textAlign: 'center'
+  }
+
+});
 
 export default App;
