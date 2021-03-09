efb1f4ef42edeac1d69802be6f3f0fc20c0f4f94
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-09 20:53:17 -0300

    Flex 1 e Flex 2 - Ocupação de espaço dentro dos apps

--- App.js
+++ App.js
@@ -6,11 +6,10 @@ class App extends Component {
   render(){
 
     return(
-      <View style={styles.area}>
-        <Text style={styles.txt1, styles.alinhaTexto}>Eu sou texto 1</Text>
-        <Text>Eu sou texto 2</Text>
-        <Text>Eu sou texto 3</Text>
-        <Text style={styles.alinhaTexto}>Eu sou texto 4</Text>
+      <View style={{backgroundColor: '#222', flex: 1}}>
+        <View style={{flex: 2, backgroundColor: 'red'}}></View>
+        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+        <View style={{flex: 2, backgroundColor: 'green'}}></View>
       </View>
     );
   }
@@ -18,17 +17,7 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  area: {
-    marginTop: 20
-  },
-  txt1: {
-    fontSize: 25,
-    color: 'red'
-  },
-  alinhaTexto: {
-    textAlign: 'center'
-  }
-
+  
 });
 
 export default App;
