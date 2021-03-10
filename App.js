8c44e77d08f6f9adf55d9fcbc920b785b1d8683d
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-09 21:11:07 -0300

    Trabalhando com TextInput do React-Native

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ''
+    };
+    this.pegarNome = this.pegarNome.bind(this);
+  }
+
+  pegarNome(texto) {
+    this.setState({
+      nome: texto
+    })
+  }
 
   render(){
 
     return(
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}>
-        <View style={{height: 50, width: 50, backgroundColor: 'red'}}></View>
-        <View style={{height: 50, width: 50, backgroundColor: 'blue'}}></View>
-        <View style={{height: 50, width: 50, backgroundColor: 'green'}}></View>
+      <View style={styles.container}>
+        <TextInput 
+        style={styles.input}
+        placeholder={"Digite seu nome: "}
+        onChangeText={this.pegarNome}
+        />
+
+      <Text style={styles.frase}>Bem Vindo, {this.state.nome}!</Text>
+
       </View>
     );
   }
@@ -17,7 +36,21 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    fontSize: 20,
+    padding: 10
+  },
+  frase: {
+    textAlign: 'center',
+    fontSize: 20
+  }
 });
 
 export default App;
