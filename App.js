33dacc107a7edfbf316db102557f8e44951836c3
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-03-11 19:25:09 -0300

    FlatList e suas props

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        {id: '1', nome: 'Daniel', idade: 20, email: 'daniel@gmail.com'},
+        {id: '2', nome: 'Marco', idade: 20, email: 'Marco@gmail.com'},
+        {id: '3', nome: 'Max', idade: 21, email: 'Max@gmail.com'},
+        {id: '4', nome: 'Japa', idade: 22, email: 'Japa@gmail.com'},
+        {id: '5', nome: 'Daniel', idade: 20, email: 'daniel@gmail.com'},
+        {id: '6', nome: 'Marco', idade: 20, email: 'Marco@gmail.com'},
+        {id: '7', nome: 'Max', idade: 21, email: 'Max@gmail.com'},
+        {id: '8', nome: 'Japa', idade: 22, email: 'Japa@gmail.com'}
+      ]
+    }
+  }
+
   
   render(){
 
     return(
       <View style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.box1}></View>
-          <View style={styles.box2}></View>
-          <View style={styles.box3}></View>
-          <View style={styles.box4}></View>
-        </ScrollView>
+        <FlatList 
+        data={this.state.feed}
+        keyExtractor={(item) => item.id}
+        renderItem={ ({item}) => <Pessoa data={item} /> }
+        >
+        </FlatList>
       </View>
     );
   }
@@ -23,22 +40,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  box1: {
-    backgroundColor: 'red',
-    height: 250
+  areaPessoa: {
+    backgroundColor: '#222',
+    marginBottom: 10,
+    height: 200
   },
-  box2: {
-    backgroundColor: 'blue',
-    height: 250
-  },
-  box3: {
-    backgroundColor: 'black',
-    height: 250
-  },
-  box4: {
-    backgroundColor: 'orange',
-    height: 250
+  txtPessoa: {
+    fontSize: 20,
+    color: '#FFF'
   }
 });
 
 export default App;
+
+
+class Pessoa extends Component {
+  render() {
+    return (
+      <View style={styles.areaPessoa}>
+        <Text style={styles.txtPessoa}>{this.props.data.nome}</Text>
+        <Text style={styles.txtPessoa}>{this.props.data.idade}</Text>
+        <Text style={styles.txtPessoa}>{this.props.data.email}</Text>
+      </View>
+    );
+  }
+}
